7b1d55062197bed323601bd8e12fd43a7c13f3bd
IgnacioNMiranda <i.miranda.f99@gmail.com>
2023-12-02 20:42:09 -0300

    feat: always use contentful for now

--- src/lib/server/cms/get-page.ts
+++ src/lib/server/cms/get-page.ts
@@ -1,11 +1,7 @@
-import { environment } from '../../config/environment';
 import { getPageBySlug } from './contentful/get-entries';
 import { contentfulPageToProps } from './contentful/page-to-props';
 
 export const getPage = async (slug: string) => {
-	if (environment.cms === 'contentful') {
-		const contentfulPage = await getPageBySlug(slug);
-		if (contentfulPage) return contentfulPageToProps(contentfulPage);
-	}
-	return null;
+	const contentfulPage = await getPageBySlug(slug);
+	if (contentfulPage) return contentfulPageToProps(contentfulPage);
 };
